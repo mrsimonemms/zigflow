3c894ded942a4f38b7607134866c3d9fa78caae7
Simon Emms <simon@simonemms.com>
2026-01-03 18:50:21 +0000

    ci: add preview webpage
    Signed-off-by: Simon Emms <simon@simonemms.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -39,10 +39,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://zigflow.dev',
+  url: process.env.ZIGFLOW_URL ?? 'https://zigflow.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.ZIGFLOW_BASE_URL ?? '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
